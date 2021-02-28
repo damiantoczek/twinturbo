706749ab9617f015d68da501d0425ec888a8c066
damiantoczek <damiantoczekprivat@gmail.com>
2021-02-28 18:47:07 +0100

    Added POST functionality.

--- index.js
+++ index.js
@@ -1,26 +1,84 @@
 const http = require('http');
 
 const app = function(config){
-  var App = {};
+  var App = {
+    routesList: [],
+    getRoutes: {},
+    postRoutes: {},
+    ERROR: {},
+    GET: null,
+    POST: null,
+    listener: null,
+    server: {}
+  };
 
-  App.GET = config.get;
-  App.POST = config.post;
+  // Cache all routes for faster checking.
+  App.routesList = [
+    ...Object.keys(config.get),
+    ...Object.keys(config.post)
+  ]
+
+  // Stored routes are stored as key:func pairs.
+  App.getRoutes = config.get;
+  App.postRoutes = config.post;
   App.ERROR = config.error;
 
+  // Handling GET/POST requests.
+  App.GET = async function(req, res){
+    var url = req.url;
+    var html = await this.getRoutes[url]();
+    res.end(html);
+  };
+
+  App.POST = async function(req, res){
+    var rawBodyString = '';
+
+    var postPromise = new Promise((success, reject) => {
+
+      req.on('error', error => {
+        console.log(error);
+        reject(error);
+      });
+
+      req.on('data', chunk => {
+        rawBodyString += chunk;
+      });
+      
+      req.on('end', () => {
+        var body = {};
+
+        // Request body to body object.
+        rawBodyString.split('&').forEach(str => {
+          let pair = str.split('=');
+          body[pair[0]] = pair[1];
+        });
+
+        console.log('SUCCESS', rawBodyString);
+        success(body);
+      });
+
+    });
+
+    var data = await postPromise;
+    var html = await this.postRoutes[req.url](data);
+    res.end(html);
+  };
+
+  // Server listener.
   App.listener = function(req,res){
     var {url, method} = req;
 
-    var route = App[method][url];
-    if(route){
-      res.end(route(req, res));
-    }else{
+    if(App.routesList.indexOf(url) === -1){
+      var errorHandler = App.ERROR[404]();
       res.statusCode = 404;
-      res.end(App.ERROR[404]());
+      res.end(errorHandler);
+    }else{
+      App[method](req, res);
     }
   };
 
+  // Initiation.
   App.server = http.createServer(App.listener);
-
   App.server.listen(config.port);
 
   return App;
@@ -36,7 +94,7 @@ const render = function(){
     if(this._values[key] === undefined || force === true){
       this._values[key] = value;
     }else{
-      console.log(`${key} key already exists, to overwrite use 'true' as third argument.`);
+      console.log(`${key} key already exists, to overwrite use 'true' as thrid argument.`);
     }
   };
 
